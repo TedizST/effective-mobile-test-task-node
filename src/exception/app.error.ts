41eddc6e3fc4729cbe435497fbafee74fa19262f
Tediz <derk.21@mail.ru>
2025-04-29 22:55:08 +0300

    refactor: delete unused module

--- src/exception/app.error.ts
+++
@@ -1,9 +0,0 @@
-export class AppError extends Error {
-  constructor(
-    public readonly method: string,
-    public readonly message: string,
-    public readonly error: Error,
-  ) {
-    super(message);
-  }
-}
