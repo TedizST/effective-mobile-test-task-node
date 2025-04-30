66ff824a04367b1b31dffec092760f80fe16db4b
Tediz <derk.21@mail.ru>
2025-04-30 20:09:36 +0300

    feat: add proccesing for business logic errors

--- src/middlewares/errorHandling.middleware.ts
+++ src/middlewares/errorHandling.middleware.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { logger } from "../utils";
 import { ResponseDTO } from "../dtos";
+import { HttpException, InvalidStatusTransitionException } from "../exceptions";
 
 export function errorHandlingMiddleware(
 	err: Error,
@@ -8,8 +9,16 @@ export function errorHandlingMiddleware(
 	res: Response,
 	_next: NextFunction
 ): void {
+	if (err instanceof HttpException) {
+		res.status(err.code).json(new ResponseDTO(false, { message: err.message }));
+		return;
+	} else if (err instanceof InvalidStatusTransitionException) {
+		res.status(400).json(new ResponseDTO(false, { message: err.message }));
+		return;
+	}
+
 	logger.error(err, "uncaught error");
 	res
-		.status(400)
+		.status(500)
 		.json(new ResponseDTO(false, { message: "internal server error" }));
 }
